31eeb9e396eb53b9cf44686d64b2ae408f7e6108
Krunal Dodiya <kunal.dodiya1@gmail.com>
2020-09-18 19:11:12 +0530

    added player options

--- src/components/Player/PlayerOptions.tsx
+++ src/components/Player/PlayerOptions.tsx
@@ -1,12 +1,66 @@
 import React, {memo} from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {useTheme} from 'styled-components';
 
 const PlayerOptions = () => {
+  const theme = useTheme();
+
   return (
-    <View style={{padding: 10}}>
-      <Text style={{color: '#000'}}>options</Text>
+    <View style={{flex: 1}}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          margin: 2,
+        }}>
+        <View style={styles.option}>
+          <Text
+            style={[
+              styles.optionText,
+              {fontFamily: theme.fontFamily.QuicksandBold},
+            ]}>
+            Download
+          </Text>
+        </View>
+
+        <View style={styles.option}>
+          <Text
+            style={[
+              styles.optionText,
+              {fontFamily: theme.fontFamily.QuicksandBold},
+            ]}>
+            240p
+          </Text>
+        </View>
+
+        <View style={styles.option}>
+          <Text
+            style={[
+              styles.optionText,
+              {fontFamily: theme.fontFamily.QuicksandBold},
+            ]}>
+            1.0x
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default memo(PlayerOptions);
+
+const styles = StyleSheet.create({
+  option: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#ccc',
+    alignItems: 'center',
+    margin: 2,
+  },
+  optionText: {
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 14,
+    textTransform: 'uppercase',
+  },
+});
