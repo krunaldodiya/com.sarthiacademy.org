651ed53137243d1fd5aeb524a5fbe42797e0d074
Krunal Dodiya <kunal.dodiya1@gmail.com>
2021-01-03 21:38:23 +0530

    latest version upgraded

--- App.tsx
+++ App.tsx
@@ -1,26 +1,11 @@
 import {StoreProvider} from 'easy-peasy';
-import React, {memo, useState} from 'react';
-import {useQuery} from 'react-query';
+import React, {memo} from 'react';
 import {ThemeProvider} from 'styled-components';
-import {authUserApi} from './src/api/authUserApi';
 import RootStackNavigator from './src/navigation/Root';
 import {store} from './src/store/store';
 import {black} from './src/themes/black';
 
 function App() {
-  const [loaded, setLoaded] = useState(false);
-
-  useQuery('auth_user', authUserApi, {
-    retry: false,
-    onSettled: () => {
-      setLoaded(true);
-    },
-  });
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
     <StoreProvider store={store}>
       <ThemeProvider theme={black}>
