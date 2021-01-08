4e413d502bbb6d54a83e39411b11df04be41857e
Krunal Dodiya <kunal.dodiya1@gmail.com>
2021-01-08 14:11:58 +0530

    added ssl

--- src/libs/vars.ts
+++ src/libs/vars.ts
@@ -1,7 +1,7 @@
 import RNFS from 'react-native-fs';
 
-export const webUrl = 'http://www.sarthiacademy.in';
-export const baseUrl = 'http://secure.sarthiacademy.in';
-export const apiUrl = 'http://secure.sarthiacademy.in/api';
+export const webUrl = 'https://www.sarthiacademy.in';
+export const baseUrl = 'https://api.sarthiacademy.in';
+export const apiUrl = 'https://api.sarthiacademy.in/api';
 
 export const downloadPath = RNFS.CachesDirectoryPath;
